8a9529c74a9c35076a2d357c3c43a66a2e8f2f5d
Rajan <Rajan@192.168.0.105>
2015-04-27 21:47:30 -0700

    Added order service.

--- src/main/java/healthkart/HealthkartOrderService.java
+++ src/main/java/healthkart/HealthkartOrderService.java
@@ -3,26 +3,29 @@
  */
 package healthkart;
 
+import java.io.InputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Rajan
  *
  */
 @Path("/healthkart")
-public class HealthkartUserService {
+public class HealthkartOrderService {
 
 	@Path("/createUser")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String createUser()
+	public Response createOrder( InputStream is )
 	{
-		return "User created";
+		return null;
 	}
 	
 	
