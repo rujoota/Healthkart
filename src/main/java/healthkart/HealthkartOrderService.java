e47f6baebe5ca735319137d0398fbd36159cbe86
Rajan <Rajan@192.168.0.105>
2015-04-27 21:46:14 -0700

    Added User Service

---
+++ src/main/java/healthkart/HealthkartOrderService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package healthkart;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Rajan
+ *
+ */
+@Path("/healthkart")
+public class HealthkartUserService {
+
+	@Path("/createUser")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String createUser()
+	{
+		return "User created";
+	}
+	
+	
+}
---
+++ src/main/java/healthkart/HealthkartUserService.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package healthkart;
+
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Rajan
+ *
+ */
+@Path("/healthkart/users")
+public class HealthkartUserService {
+
+	@Path("/createUser")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUser( InputStream is )
+	{
+		return null;
+	}
+	
+	@Path("/retrieveUser")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String retrieveUser( @HeaderParam("userId") String userId )
+	{
+		
+		return "user";
+	}
+	
+	@Path("/updateUser")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String updateUser( @PathParam("userId") String userId, InputStream is )
+	{
+		return "user";
+	}
+	
+	@Path("/deleteUser")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String deleteUser( @PathParam("userId") String userId )
+	{
+		return "user";
+	}
+	
+}
